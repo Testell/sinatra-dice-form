4e5433baf61efb3beac11768a1e0738cf96e268f
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-12 20:35:34 +0000

    finished

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ get("/") do
   
   erb(:home)
 end
+
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+
+  @num_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
+  erb(:roll)
+end
